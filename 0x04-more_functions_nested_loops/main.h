ef07a115a7ce794108f3bbad27b7d56b2cfa64a2
Alexander <oguejioforalexander@gmail.com>
2022-09-16 02:50:31 -0700

    header

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+/**
+ * File: main.h
+ * Author: Alexander Oguejiofor
+ * Desc: Header file containing function declarations
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+int _isupper(int c);
+int _isdigit(int c);
+int _putchar(char c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
+
+#endif /* MAIN_H */
