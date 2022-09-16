cdd02ae0ca74734e1e981e18fa00321b5555567c
Alexander <oguejioforalexander@gmail.com>
2022-09-16 02:53:17 -0700

    first batch

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - print all numbers from 0 to 9
+ * Return: void
+ */
+void print_numbers(void)
+{
+	int number;
+
+	for (number = 48; number <= 57; number++)
+		_putchar(number);
+
+	_putchar('\n');
+}
