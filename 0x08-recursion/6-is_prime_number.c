d8d447987d37bd9476b75269ade385f7017c63f5
Alexander <oguejioforalexander@gmail.com>
2022-09-28 12:21:18 -0700

    prime

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -16,3 +16,16 @@ int prime2(int a, int b)
 		return (0);
 	return (prime2(a, b + 1));
 }
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: Number Integer
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (prime2(n, 2));
+}
