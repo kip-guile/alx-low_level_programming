29a6fb6d182ceaf352accef3f8a18bc805c7175b
Alexander <oguejioforalexander@gmail.com>
2022-09-28 12:15:01 -0700

    rev

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * _strlen_recursion - Returns Length of String
+ * _print_rev_recursion - Prints a string in reverse
  * @s: string
  *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
-int _strlen_recursion(char *s)
+void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
 	{
-		return (1 + _strlen_recursion(s + 1));
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
 	}
-	return (0);
 }
